3f6c1764ed8d6e70660342e7bf341b3ea2aaa25b
Ekta017 <ektapardeshi8@gmail.com>
2026-05-02 18:06:59 +0530

    added gunicorn

--- movie_booking/settings.py
+++ movie_booking/settings.py
@@ -79,13 +79,11 @@ WSGI_APPLICATION = "movie_booking.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
